bd5b33fac3728e37dd54ae16a56cd7b023a4d054
Ram Patra <ramswarooppatra@gmail.com>
2020-03-20 10:53:05 +0000

    Initial Commit

---
+++ Dad Jokes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dad Jokes
+//
+//  Created by Ram Patra on 20/03/2020.
+//  Copyright © 2020 Ram Patra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
